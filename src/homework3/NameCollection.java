76530d3605ead73d16545417d2d8d2d6b48088c8
al3xka1n3 <al3xka1n3@gmail.com>
2017-11-28 13:15:54 +0200

    Collections

--- src/homework3/NameCollection.java
+++ src/homework3/NameCollection.java
@@ -1,12 +1,14 @@
 package homework3;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 /**
  * Created by Oleksandr.Lokhtin on 27-Nov-17.
  */
-public class Collections {
+public class NameCollection {
 
     public static void main(String[] args) {
         List<String> nameList = new ArrayList<String>() {{
@@ -20,6 +22,21 @@ public class Collections {
 
         nameList.remove("John");
         isExist(nameList, "John");
+
+        Iterator<String> iteratorBeforeSort = nameList.iterator();
+        System.out.println("Before sorting: ");
+        while (iteratorBeforeSort.hasNext()) {
+            System.out.println(iteratorBeforeSort.next());
+        }
+        System.out.println("\n");
+
+        Collections.sort(nameList);
+        Iterator<String> iteratorAfterSort = nameList.iterator();
+        System.out.println("After sorting: ");
+        while (iteratorAfterSort.hasNext()) {
+            System.out.println(iteratorAfterSort.next());
+        }
+
     }
 
     private static void isExist(List<String> nameList, String verifiableName) {
